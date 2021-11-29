e93b9a35e22da619fa4012f56aaee3a757870d16
Pierre <pierre.pellegrino@laposte.net>
2021-11-29 11:41:59 +0100

    Ajout seeds (Création Users, items, cart, orders)

--- db/schema.rb
+++ db/schema.rb
@@ -64,7 +64,7 @@ ActiveRecord::Schema.define(version: 2021_11_29_094130) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.boolean "is_admin"
+    t.boolean "is_admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,55 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+### Nettoyage de la BDD
+User.destroy_all
+Item.destroy_all
+Cart.destroy_all
+Order.destroy_all
+JoinTableItemOrder.destroy_all
+JoinTableItemCart.destroy_all
+
+### Remplissage de la BDD
+
+# Création des users
+User.create(email: "a@a.fr", password: "adminpwd", is_admin: true)
+user = User.create(email: "u@u.fr", password: "userpwd")
+
+picturesArr = ["https://s3.amazonaws.com/api.coolcatsnft.com/thumbnails/0_thumbnail.png", 
+                "https://s3.amazonaws.com/api.coolcatsnft.com/thumbnails/5_thumbnail.png",
+                "https://s3.amazonaws.com/api.coolcatsnft.com/thumbnails/4_thumbnail.png",
+                "https://s3.amazonaws.com/api.coolcatsnft.com/thumbnails/8_thumbnail.png",
+                "https://s3.amazonaws.com/api.coolcatsnft.com/thumbnails/11_thumbnail.png",
+                "https://s3.amazonaws.com/api.coolcatsnft.com/thumbnails/18_thumbnail.png",
+                "https://s3.amazonaws.com/api.coolcatsnft.com/thumbnails/22_thumbnail.png",
+                "https://s3.amazonaws.com/api.coolcatsnft.com/thumbnails/28_thumbnail.png",
+                "https://s3.amazonaws.com/api.coolcatsnft.com/thumbnails/31_thumbnail.png"
+              ]
+
+# Création des items
+9.times do |i|
+  Item.create(title: "Photo n°#{i}", 
+    description: "Description n°#{i}", 
+    price: Faker::Number.between(from: 10.0, to: 150.0).round(2), 
+    image_url: picturesArr[i]
+  )
+end
+
+# Création panier / join table
+userCart = Cart.create(user: user)
+
+4.times do |i|
+  JoinTableItemCart.create(cart: userCart, item: Item.all[i])
+end
+
+
+# Création orders / join table
+userOrder1, userOrder2 = Order.create(user: user)
+
+4.times do |i|
+  JoinTableItemOrder.create(order: userOrder1, item: Item.all[i])
+  JoinTableItemOrder.create(order: userOrder2, item: Item.all[i+4])
+end
+
