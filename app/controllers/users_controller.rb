defbf6bd12f917fbd777ba7242a9d734d12a456f
Beygs <boris.gilles@hotmail.fr>
2021-11-29 18:05:07 +0100

    Show user done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, :check_user
+
   def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def check_user
+    if current_user != User.find(params[:id])
+      redirect_to root_path
+    end
   end
 end
