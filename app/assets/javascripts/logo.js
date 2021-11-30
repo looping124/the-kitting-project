9e546ea760ce64f68b402c9d1f713c8fbb4f53d3
Beygs <boris.gilles@hotmail.fr>
2021-11-30 11:06:20 +0100

    nav colors changed

---
+++ app/assets/javascripts/logo.js
@@ -0,0 +1,59 @@
+const coolCatsReactions = [
+  'blank',
+  'cool',
+  'cupcake',
+  'deepsea',
+  'dizzy',
+  'dizzy2',
+  'doubleface',
+  'dragon',
+  'fancy',
+  'firecrown',
+  'firehalo',
+  'froggy',
+  'glossy',
+  'handsome',
+  'hearmeout',
+  'hobbes',
+  'kewlcet',
+  'kiss',
+  'knight',
+  'lol',
+  'love',
+  'metamask',
+  'navy',
+  'nosignal',
+  'ohno',
+  'pirate',
+  'prince',
+  'ramcowboy',
+  'sleep',
+  'smiley',
+  'smirk',
+  'smirk2',
+  'sob',
+  'squad',
+  'sushi',
+  'sweat_smile',
+  'tongue',
+  'uuh',
+  'what',
+  'wink'
+]
+
+function chooseRandomLogo() {
+  const imgId = Math.floor(Math.random() * coolCatsReactions.length)
+  const img = coolCatsReactions[imgId]
+
+  const logoDiv = document.querySelector('.navbar__brand .logo');
+
+  logoDiv.setAttribute(
+    'style',
+    `background-image: url(https://www.coolcatsnft.com/assets/images/e_${img}.png);
+    background-position: 50% 50%;
+    background-size: cover;
+    transform: scaleX(-1);
+  `)
+}
+
+chooseRandomLogo();
