76095a1530382db1e974f9c3d034de1140526d96
Cedric Le Net <lenetcedric@gmail.com>
2021-11-30 09:40:31 +0100

    after_create dans model order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,25 @@
 class Order < ApplicationRecord
+
+  after_create :fill_order
+  after_create :empty_cart
+
   #Associations
   belongs_to :user
   has_many :join_table_item_orders
   has_many :items, through: :join_table_item_orders
+
+  #on remplit la commande avec la join table du panier
+  def fill_order
+    self.user.join_table_item_carts.each do |join_table_item_cart|
+      JoinTableItemOrder.create(order: self, item: join_table_item_cart.item)
+    end
+  end
+
+  #on vide le panier
+  def empty_cart
+    self.user.join_table_item_carts.destroy_all
+  end
+
+end
+
 end
