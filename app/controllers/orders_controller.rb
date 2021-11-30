6e392355b4283bc52ef016032647adff9af9de5a
Cedric Le Net <lenetcedric@gmail.com>
2021-11-30 16:21:14 +0100

    show order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,23 +1,28 @@
 class OrdersController < ApplicationController
+ 
   def index
+    @orders = Order.where(user_id: current_user.id)
   end
 
-  def new
-  end
+  # def new
+  # end
 
   def create
     Order.create(user: current_user)
   end
 
   def show
+    @order = Order.find(params[:id])
   end
+  # #For admin only :
+  # def edit
+  # end
 
-  def edit
-  end
+  # def update
+  # end
 
-  def update
-  end
+  # def destroy
+  # end
 
-  def destroy
-  end
+
 end
