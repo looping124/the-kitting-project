50ef4e1f16c7d9940fb4c4e21623117fd87532c4
Cedric Le Net <lenetcedric@gmail.com>
2021-11-30 16:51:06 +0100

    mes commandes dans mon compte

--- app/controllers/join_table_item_carts_controller.rb
+++ app/controllers/join_table_item_carts_controller.rb
@@ -1,7 +1,7 @@
 class JoinTableItemCartsController < ApplicationController
   def create
     JoinTableItemCart.create(cart: current_user.cart, item: Item.find(params[:id]))
-    redirect_to item_path(params[:id])
+    redirect_to root_path
   end
 
   def destroy
