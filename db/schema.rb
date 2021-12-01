b038b23c7385517eaa5d09d2d48856ed41aab5dc
Beygs <boris.gilles@hotmail.fr>
2021-12-01 11:23:29 +0100

    céfé

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_30_141202) do
+ActiveRecord::Schema.define(version: 2021_12_01_100203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2021_11_30_141202) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "price"
+    t.string "stripe_price_id"
   end
 
   create_table "join_table_item_carts", force: :cascade do |t|
