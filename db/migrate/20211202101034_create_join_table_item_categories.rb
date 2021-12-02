315bd24ffa545ee75ed27da333d77793a6fa5008
Cedric Le Net <lenetcedric@gmail.com>
2021-12-02 11:11:24 +0100

    models dans migrations

--- db/migrate/20211202101034_create_join_table_item_categories.rb
+++ db/migrate/20211202101034_create_join_table_item_categories.rb
@@ -1,6 +1,6 @@
-class CreateJoinTableCategoriesItems < ActiveRecord::Migration[5.2]
+class CreateJoinTableItemCategories < ActiveRecord::Migration[5.2]
   def change
-    create_table :join_table_categories_items do |t|
+    create_table :join_table_item_categories do |t|
       t.belongs_to :item, index: true
       t.belongs_to :category, index: true
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_02_100110) do
+ActiveRecord::Schema.define(version: 2021_12_02_101034) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,15 +60,6 @@ ActiveRecord::Schema.define(version: 2021_12_02_100110) do
     t.boolean "sellable", default: true
   end
 
-  create_table "join_table_categories_items", force: :cascade do |t|
-    t.bigint "item_id"
-    t.bigint "category_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["category_id"], name: "index_join_table_categories_items_on_category_id"
-    t.index ["item_id"], name: "index_join_table_categories_items_on_item_id"
-  end
-
   create_table "join_table_item_carts", force: :cascade do |t|
     t.integer "quantity", default: 1
     t.bigint "item_id"
@@ -77,6 +68,15 @@ ActiveRecord::Schema.define(version: 2021_12_02_100110) do
     t.datetime "updated_at", null: false
     t.index ["cart_id"], name: "index_join_table_item_carts_on_cart_id"
     t.index ["item_id"], name: "index_join_table_item_carts_on_item_id"
+  end
+
+  create_table "join_table_item_categories", force: :cascade do |t|
+    t.bigint "item_id"
+    t.bigint "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_join_table_item_categories_on_category_id"
+    t.index ["item_id"], name: "index_join_table_item_categories_on_item_id"
   end
 
   create_table "join_table_item_orders", force: :cascade do |t|
